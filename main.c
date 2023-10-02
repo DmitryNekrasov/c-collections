d984a342d0179d5463591c8cccfeda398d1d3930
Dmitry Nekrasov <scannorone@gmail.com>
2023-10-02 14:15:34 +0400

    Add Lorem ipsum example

--- main.c
+++ main.c
@@ -1,53 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "hash_map.h"
 
-int main() {
-    struct hash_map* map = init_hash_map();
+#define MAX_STR_NUMBER 500
+#define MAX_LEN 2000
+#define DELIMITERS " .,!?:;\n"
 
-    put(map, "hello", 8);
-    put(map, "bgfdb", 4);
-    put(map, "vthv", 3);
-    put(map, "fgf", 10);
-    put(map, "fghfg", 101);
-    put(map, "vgvsd", 56);
-    put(map, "hfgjfhj", 123);
-    put(map, "sgjdhjhf", 21);
-    put(map, "test", 1);
-    put(map, "test", 2);
-    put(map, "test", 3);
-    put(map, "test", 4);
-    put(map, "test", 5);
-    put(map, "helslo", 8);
-    put(map, "bgfsdb", 4);
-    put(map, "vtdfhv", 3);
-    put(map, "fgdfqrgf", 10);
-    put(map, "fghfqg", 101);
-    put(map, "vgvqsd", 56);
-    put(map, "hfgjqfhj", 123);
-    put(map, "sgjdsdhjhf", 21);
-    put(map, "testgb", 1);
-    put(map, "tesnmt", 2);
-    put(map, "tesnjt", 3);
-    put(map, "tesdsdsdt", 4);
-    put(map, "tesbbbt", 5);
+char text[MAX_STR_NUMBER][MAX_LEN];
 
-    printf("size = %d\n", size(map));
-
-    printf("contains = %d\n", contains(map, "hello"));
-    printf("contains = %d\n", contains(map, "hello!"));
-    printf("contains = %d\n", contains(map, "hfgjfhjdfgn!"));
-    printf("contains = %d\n", contains(map, "sdfdsc"));
-    printf("contains = %d\n", contains(map, "test"));
-    printf("contains = %d\n", contains(map, "sssasas"));
-    printf("contains = %d\n", contains(map, "qwwww"));
-
-    printf("get = %d\n\n", get(map, "fghfg"));
-
-    print_map_debug(map);
-    print_map(map);
-
-    destroy_map(map);
-
+int main(int argc, char* argv[]) {
+    if (argc == 2) {
+        const char* path = argv[1];
+        FILE* file = fopen(path, "r");
+        if (file != NULL) {
+            int index = 0;
+            int token_count = 0;
+            struct hash_map* map = init_hash_map();
+            while (fgets(text[index], MAX_LEN, file)) {
+                char* str = text[index];
+                char* token = strtok(str, DELIMITERS);
+                while (token) {
+                    int count = contains(map, token) ? get(map, token) : 0;
+                    put(map, token, count + 1);
+                    token = strtok(NULL, DELIMITERS);
+                    token_count++;
+                }
+                index++;
+            }
+            print_map_debug(map);
+            print_map(map);
+            printf("size = %d\n", size(map));
+            printf("token_count = %d\n", token_count);
+            destroy_map(map);
+            fclose(file);
+        }
+    }
     return 0;
 }
