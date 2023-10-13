75fa8be42d47d88f1ffa8f19a14fd27fd6247f8c
Dmitry Nekrasov <scannorone@gmail.com>
2023-10-13 19:16:40 +0400

    Add array_list documentation

--- array_list.h
+++ array_list.h
@@ -5,6 +5,9 @@
 
 struct __base_list;
 
+/**
+ * Resizable-array of generic type
+ */
 #define array_list(type) \
     struct {             \
         int __size;      \
@@ -13,9 +16,28 @@ struct __base_list;
     } *
 
 struct __base_list* __new_array_list(int element_size);
+
+/**
+ * Destroy this list
+ *
+ * @param this this list
+ */
 void delete_array_list(struct __base_list* this);
+
+/**
+ * Returns the number of elements in this list.
+ *
+ * @param this this list
+ * @return the number of elements in this list
+ */
 int al_size(struct __base_list* this);
 
+/**
+ * Print a string representation of this list
+ *
+ * @param this this list
+ * @param print_element list item print function
+ */
 #define al_print(this, print_element)                                        \
     printf("[");                                                             \
     if (this->__size > 0) {                                                  \
@@ -28,10 +50,19 @@ int al_size(struct __base_list* this);
     }                                                                        \
     printf("]\n")
 
+/**
+ * Constructs an empty list with an initial capacity of eight.
+ */
 #define new_array_list(type) (array_list(type)) __new_array_list(sizeof(type))
 
 void __al_resize(struct __base_list* this, int new_capacity, int element_size);
 
+/**
+ * Appends the specified element to the end of this list.
+ *
+ * @param this this list
+ * @param value element to be appended to this list
+ */
 #define al_add(this, value)                                     \
     if (this->__size == this->__capacity) {                     \
         __al_resize(this, this->__capacity * 2, sizeof(value)); \
@@ -39,6 +70,13 @@ void __al_resize(struct __base_list* this, int new_capacity, int element_size);
     this->__data[this->__size] = value;                         \
     this->__size++
 
+/**
+ * Returns the element at the specified position in this list.
+ *
+ * @param this this list
+ * @param index index of the element to return
+ * @return the element at the specified position in this list
+ */
 #define al_at(this, index) this->__data[index]
 
 #endif //HASH_MAP_ARRAY_LIST_H
