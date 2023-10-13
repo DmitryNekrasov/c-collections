83b06bdebd37e02aab6a4b930392f810f40f94dc
Dmitry Nekrasov <scannorone@gmail.com>
2023-10-13 21:21:15 +0400

    Add alSize tests

--- test/ArrayListSuite.c
+++ test/ArrayListSuite.c
@@ -1,13 +1,39 @@
 #include "../CuTest/CuTest.h"
 
-void TestArrayListSize(CuTest* tc) {
-    CuAssertTrue(tc, 1);
+#include "../ArrayList/array_list.h"
+
+void TestArrayListSizeEmptyList(CuTest* tc) {
+    ArrayList(int) list = newArrayList(int);
+
+    CuAssertIntEquals(tc, alSize(list), 0);
+}
+
+void TestArrayListSizeBeforeRealloc(CuTest* tc) {
+    ArrayList(int) list = newArrayList(int);
+    int n = 7;
+    for (int i = 0; i < n; i++) {
+        alAdd(list, i);
+    }
+
+    CuAssertIntEquals(tc, alSize(list), n);
+}
+
+void TestArrayListSizeAfterRealloc(CuTest* tc) {
+    ArrayList(int) list = newArrayList(int);
+    int n = 100;
+    for (int i = 0; i < n; i++) {
+        alAdd(list, i);
+    }
+
+    CuAssertIntEquals(tc, alSize(list), n);
 }
 
 CuSuite* CuGetArrayListSuite() {
     CuSuite* suite = CuSuiteNew();
 
-    SUITE_ADD_TEST(suite, TestArrayListSize);
+    SUITE_ADD_TEST(suite, TestArrayListSizeEmptyList);
+    SUITE_ADD_TEST(suite, TestArrayListSizeBeforeRealloc);
+    SUITE_ADD_TEST(suite, TestArrayListSizeAfterRealloc);
 
     return suite;
 }
